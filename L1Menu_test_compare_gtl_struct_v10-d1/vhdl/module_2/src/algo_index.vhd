8b56097ccfd3e25f130d8a3881b076e53be1b40b
Herbert Bergauer <herbert.bergauer@oeaw.ac.at>
2020-01-22 10:45:15 +0100

    added new menu

---
+++ L1Menu_test_compare_gtl_struct_v10-d1/vhdl/module_2/src/algo_index.vhd
@@ -0,0 +1,30 @@
+-- ========================================================
+-- from VHDL producer:
+
+-- Module ID: 2
+
+-- Name of L1 Trigger Menu:
+-- L1Menu_test_compare_gtl_struct_v10
+
+-- Unique ID of L1 Trigger Menu:
+-- 54037b61-3cb6-4e40-b473-7abdb11873f1
+
+-- Unique ID of firmware implementation:
+-- d00fc042-f78d-4eb0-a252-e4515f2fada8
+
+-- Scale set:
+-- scales_2017_05_23
+
+-- VHDL producer version
+-- v2.7.3
+
+-- HB 2016-09-16: constants for algo_mapping_rop.
+type global_index_array is array (0 to NR_ALGOS-1) of integer;
+constant global_index: global_index_array := (
+          0, -- module_index: 0, name: L1_test_0
+         17, -- module_index: 1, name: L1_test_17
+         10, -- module_index: 2, name: L1_test_10
+    others => 0
+);
+
+-- ========================================================
