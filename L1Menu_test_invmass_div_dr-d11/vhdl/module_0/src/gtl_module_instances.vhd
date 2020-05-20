537dc1bde45c883bf9221df642e724e481ce8c59
herbberg <herbert.bergauer@oeaw.ac.at>
2020-05-20 09:57:35 +0200

    bug fixed

--- L1Menu_test_invmass_div_dr-d11/vhdl/module_0/src/gtl_module_instances.vhd
+++ L1Menu_test_invmass_div_dr-d11/vhdl/module_0/src/gtl_module_instances.vhd
@@ -29,48 +29,48 @@
 -- Instantiations of eta and phi conversion to muon scale for calo-muon and muon-esums correlation conditions (used for DETA, DPHI, DR and mass) - once for every calo ObjectType in certain Bx used in correlation conditions
 
 -- Instantiations of pt, eta, phi, cos-phi and sin-phi for correlation conditions (used for DETA, DPHI, DR, mass, overlap_remover and b_tagging) - once for every ObjectType in certain Bx used in correlation conditions
-    muon_data_bx_0_l: for i in 0 to NR_MUON_OBJECTS-1 generate
-        muon_pt_integer_bx_0(i) <= MUON_PT_LUT(CONV_INTEGER(muon_bx_0(i)(D_S_I_MUON_V2.et_high downto D_S_I_MUON_V2.et_low)));
-        muon_pt_vector_bx_0(i)(MUON_PT_VECTOR_WIDTH-1 downto 0) <= CONV_STD_LOGIC_VECTOR(MUON_PT_LUT(CONV_INTEGER(muon_bx_0(i)(D_S_I_MUON_V2.et_high downto D_S_I_MUON_V2.et_low))), MUON_PT_VECTOR_WIDTH);
-        muon_eta_integer_bx_0(i) <= CONV_INTEGER(signed(muon_bx_0(i)(D_S_I_MUON_V2.eta_high downto D_S_I_MUON_V2.eta_low)));
-        muon_phi_integer_bx_0(i) <= CONV_INTEGER(muon_bx_0(i)(D_S_I_MUON_V2.phi_high downto D_S_I_MUON_V2.phi_low));
-        muon_cos_phi_bx_0(i) <= MUON_COS_PHI_LUT(CONV_INTEGER(muon_bx_0(i)(D_S_I_MUON_V2.phi_high downto D_S_I_MUON_V2.phi_low)));
-        muon_sin_phi_bx_0(i) <= MUON_SIN_PHI_LUT(CONV_INTEGER(muon_bx_0(i)(D_S_I_MUON_V2.phi_high downto D_S_I_MUON_V2.phi_low)));
-    end generate muon_data_bx_0_l;
+    mu_data_bx_0_l: for i in 0 to NR_MU_OBJECTS-1 generate
+        mu_pt_integer_bx_0(i) <= MU_PT_LUT(CONV_INTEGER(mu_bx_0(i)(D_S_I_MU_V2.et_high downto D_S_I_MU_V2.et_low)));
+        mu_pt_vector_bx_0(i)(MU_PT_VECTOR_WIDTH-1 downto 0) <= CONV_STD_LOGIC_VECTOR(MU_PT_LUT(CONV_INTEGER(mu_bx_0(i)(D_S_I_MU_V2.et_high downto D_S_I_MU_V2.et_low))), MU_PT_VECTOR_WIDTH);
+        mu_eta_integer_bx_0(i) <= CONV_INTEGER(signed(mu_bx_0(i)(D_S_I_MU_V2.eta_high downto D_S_I_MU_V2.eta_low)));
+        mu_phi_integer_bx_0(i) <= CONV_INTEGER(mu_bx_0(i)(D_S_I_MU_V2.phi_high downto D_S_I_MU_V2.phi_low));
+        mu_cos_phi_bx_0(i) <= MU_COS_PHI_LUT(CONV_INTEGER(mu_bx_0(i)(D_S_I_MU_V2.phi_high downto D_S_I_MU_V2.phi_low)));
+        mu_sin_phi_bx_0(i) <= MU_SIN_PHI_LUT(CONV_INTEGER(mu_bx_0(i)(D_S_I_MU_V2.phi_high downto D_S_I_MU_V2.phi_low)));
+    end generate mu_data_bx_0_l;
 
 -- Instantiations of differences for correlation conditions (used for DETA, DPHI, DR, mass and b_tagging) - once for correlation conditions with two ObjectTypes in certain Bxs
-    muon_muon_diff_eta_bx_0_bx_0_i: entity work.sub_eta_integer_obj_vs_obj
-        generic map(NR_MUON_OBJECTS, NR_MUON_OBJECTS)
-        port map(muon_eta_integer_bx_0, muon_eta_integer_bx_0, muon_muon_bx_0_bx_0_diff_eta_integer);
-    muon_muon_diff_phi_bx_0_bx_0_i: entity work.sub_phi_integer_obj_vs_obj
-        generic map(NR_MUON_OBJECTS, NR_MUON_OBJECTS, MUON_PHI_HALF_RANGE_BINS)
-        port map(muon_phi_integer_bx_0, muon_phi_integer_bx_0, muon_muon_bx_0_bx_0_diff_phi_integer);
-    muon_muon_bx_0_bx_0_l1: for i in 0 to NR_MUON_OBJECTS-1 generate
-        muon_muon_bx_0_bx_0_l2: for j in 0 to NR_MUON_OBJECTS-1 generate
-            muon_muon_bx_0_bx_0_diff_eta_integer_lut(i,j) <= MUON_MUON_DIFF_ETA_LUT(muon_muon_bx_0_bx_0_diff_eta_integer(i,j));
-            muon_muon_bx_0_bx_0_diff_eta_vector(i,j) <= CONV_STD_LOGIC_VECTOR(MUON_MUON_DIFF_ETA_LUT(muon_muon_bx_0_bx_0_diff_eta_integer(i,j)),DETA_DPHI_VECTOR_WIDTH_ALL);
-            muon_muon_bx_0_bx_0_diff_phi_integer_lut(i,j) <= MUON_MUON_DIFF_PHI_LUT(muon_muon_bx_0_bx_0_diff_phi_integer(i,j));
-            muon_muon_bx_0_bx_0_diff_phi_vector(i,j) <= CONV_STD_LOGIC_VECTOR(MUON_MUON_DIFF_PHI_LUT(muon_muon_bx_0_bx_0_diff_phi_integer(i,j)),DETA_DPHI_VECTOR_WIDTH_ALL);
-            muon_muon_bx_0_bx_0_deta_bin_vector(i,j) <= CONV_STD_LOGIC_VECTOR(muon_muon_bx_0_bx_0_diff_eta_integer(i,j),MUON_DETA_BINS_WIDTH);
-            muon_muon_bx_0_bx_0_dphi_bin_vector(i,j) <= CONV_STD_LOGIC_VECTOR(muon_muon_bx_0_bx_0_diff_phi_integer(i,j),MUON_DPHI_BINS_WIDTH);
-        end generate muon_muon_bx_0_bx_0_l2;
-    end generate muon_muon_bx_0_bx_0_l1;
+    mu_mu_diff_eta_bx_0_bx_0_i: entity work.sub_eta_integer_obj_vs_obj
+        generic map(NR_MU_OBJECTS, NR_MU_OBJECTS)
+        port map(mu_eta_integer_bx_0, mu_eta_integer_bx_0, mu_mu_bx_0_bx_0_diff_eta_integer);
+    mu_mu_diff_phi_bx_0_bx_0_i: entity work.sub_phi_integer_obj_vs_obj
+        generic map(NR_MU_OBJECTS, NR_MU_OBJECTS, MU_PHI_HALF_RANGE_BINS)
+        port map(mu_phi_integer_bx_0, mu_phi_integer_bx_0, mu_mu_bx_0_bx_0_diff_phi_integer);
+    mu_mu_bx_0_bx_0_l1: for i in 0 to NR_MU_OBJECTS-1 generate
+        mu_mu_bx_0_bx_0_l2: for j in 0 to NR_MU_OBJECTS-1 generate
+            mu_mu_bx_0_bx_0_diff_eta_integer_lut(i,j) <= MU_MU_DIFF_ETA_LUT(mu_mu_bx_0_bx_0_diff_eta_integer(i,j));
+            mu_mu_bx_0_bx_0_diff_eta_vector(i,j) <= CONV_STD_LOGIC_VECTOR(MU_MU_DIFF_ETA_LUT(mu_mu_bx_0_bx_0_diff_eta_integer(i,j)),DETA_DPHI_VECTOR_WIDTH_ALL);
+            mu_mu_bx_0_bx_0_diff_phi_integer_lut(i,j) <= MU_MU_DIFF_PHI_LUT(mu_mu_bx_0_bx_0_diff_phi_integer(i,j));
+            mu_mu_bx_0_bx_0_diff_phi_vector(i,j) <= CONV_STD_LOGIC_VECTOR(MU_MU_DIFF_PHI_LUT(mu_mu_bx_0_bx_0_diff_phi_integer(i,j)),DETA_DPHI_VECTOR_WIDTH_ALL);
+            mu_mu_bx_0_bx_0_deta_bin_vector(i,j) <= CONV_STD_LOGIC_VECTOR(mu_mu_bx_0_bx_0_diff_eta_integer(i,j),MU_DETA_BINS_WIDTH);
+            mu_mu_bx_0_bx_0_dphi_bin_vector(i,j) <= CONV_STD_LOGIC_VECTOR(mu_mu_bx_0_bx_0_diff_phi_integer(i,j),MU_DPHI_BINS_WIDTH);
+        end generate mu_mu_bx_0_bx_0_l2;
+    end generate mu_mu_bx_0_bx_0_l1;
 
 -- Instantiations of cosh-deta and cos-dphi LUTs for correlation conditions (used for mass and overlap_remover) - once for correlation conditions with two ObjectTypes in certain Bxs
-    muon_muon_bx_0_bx_0_cosh_cos_l1: for i in 0 to NR_MUON_OBJECTS-1 generate
-        muon_muon_bx_0_bx_0_cosh_cos_l2: for j in 0 to NR_MUON_OBJECTS-1 generate
-            muon_muon_bx_0_bx_0_cosh_deta_vector(i,j) <= CONV_STD_LOGIC_VECTOR(MUON_MUON_COSH_DETA_LUT(muon_muon_bx_0_bx_0_diff_eta_integer(i,j)), MUON_MUON_COSH_COS_VECTOR_WIDTH);
-            muon_muon_bx_0_bx_0_cos_dphi_vector(i,j) <= CONV_STD_LOGIC_VECTOR(MUON_MUON_COS_DPHI_LUT(muon_muon_bx_0_bx_0_diff_phi_integer(i,j)), MUON_MUON_COSH_COS_VECTOR_WIDTH);
-            muon_muon_bx_0_bx_0_cosh_deta_integer(i,j) <= MUON_MUON_COSH_DETA_LUT(muon_muon_bx_0_bx_0_diff_eta_integer(i,j));
-            muon_muon_bx_0_bx_0_cos_dphi_integer(i,j) <= MUON_MUON_COS_DPHI_LUT(muon_muon_bx_0_bx_0_diff_phi_integer(i,j));
-        end generate muon_muon_bx_0_bx_0_cosh_cos_l2;
-    end generate muon_muon_bx_0_bx_0_cosh_cos_l1;
+    mu_mu_bx_0_bx_0_cosh_cos_l1: for i in 0 to NR_MU_OBJECTS-1 generate
+        mu_mu_bx_0_bx_0_cosh_cos_l2: for j in 0 to NR_MU_OBJECTS-1 generate
+            mu_mu_bx_0_bx_0_cosh_deta_vector(i,j) <= CONV_STD_LOGIC_VECTOR(MU_MU_COSH_DETA_LUT(mu_mu_bx_0_bx_0_diff_eta_integer(i,j)), MU_MU_COSH_COS_VECTOR_WIDTH);
+            mu_mu_bx_0_bx_0_cos_dphi_vector(i,j) <= CONV_STD_LOGIC_VECTOR(MU_MU_COS_DPHI_LUT(mu_mu_bx_0_bx_0_diff_phi_integer(i,j)), MU_MU_COSH_COS_VECTOR_WIDTH);
+            mu_mu_bx_0_bx_0_cosh_deta_integer(i,j) <= MU_MU_COSH_DETA_LUT(mu_mu_bx_0_bx_0_diff_eta_integer(i,j));
+            mu_mu_bx_0_bx_0_cos_dphi_integer(i,j) <= MU_MU_COS_DPHI_LUT(mu_mu_bx_0_bx_0_diff_phi_integer(i,j));
+        end generate mu_mu_bx_0_bx_0_cosh_cos_l2;
+    end generate mu_mu_bx_0_bx_0_cosh_cos_l1;
 
 -- Instantiations of conditions
 invariant_mass_i0_i: entity work.muon_muon_mass_div_dr_condition
     generic map(
         true,
-        MUON_PT_VECTOR_WIDTH, MUON_MUON_COSH_COS_VECTOR_WIDTH, MUON_MUON_INV_DR_SQ_VECTOR_WIDTH,
+        MU_PT_VECTOR_WIDTH, MU_MU_COSH_COS_VECTOR_WIDTH, MU_INV_DR_SQ_VECTOR_WIDTH,
         0, 7, true,
         X"0014",
         0, 
@@ -95,15 +95,15 @@ invariant_mass_i0_i: entity work.muon_muon_mass_div_dr_condition
         "ign", X"FF00", X"F",     
         X"00000000800000000000", X"00000000000000008000"
     )
-    port map(lhc_clk, muon_bx_0, muon_bx_0,
-        muon_muon_bx_0_bx_0_deta_bin_vector, muon_muon_bx_0_bx_0_dphi_bin_vector,
-        muon_pt_vector_bx_0, muon_pt_vector_bx_0,
-        muon_muon_bx_0_bx_0_cosh_deta_vector, muon_muon_bx_0_bx_0_cos_dphi_vector,
+    port map(lhc_clk, mu_bx_0, mu_bx_0,
+        mu_mu_bx_0_bx_0_deta_bin_vector, mu_mu_bx_0_bx_0_dphi_bin_vector,
+        mu_pt_vector_bx_0, mu_pt_vector_bx_0,
+        mu_mu_bx_0_bx_0_cosh_deta_vector, mu_mu_bx_0_bx_0_cos_dphi_vector,
         invariant_mass_i0);
 
 -- Instantiations of algorithms
 
--- 0 L1_muon_invmass_div_dr : mass_inv{MUON10,MUON20}[MASS_MASS_7to18]
+-- 0 L1_muon_invmass_div_dr : mass_inv{MU10,MU20}[MASS_MASS_7to18]
 l1_muon_invmass_div_dr <= invariant_mass_i0;
 algo(0) <= l1_muon_invmass_div_dr;
 
--- L1Menu_test_invmass_div_dr-d11/vhdl/module_0/src/gtl_module_signals.vhd
+++ L1Menu_test_invmass_div_dr-d11/vhdl/module_0/src/gtl_module_signals.vhd
@@ -24,30 +24,28 @@
 
 -- Signal definition of pt, eta and phi for correlation conditions.
 -- Insert "signal_correlation_conditions_pt_eta_phi_cos_sin_phi.vhd.j2" as often as an ObjectType at a certain Bx is used in a correlation condition.
-    signal muon_pt_integer_bx_0: diff_integer_inputs_array(0 to NR_MUON_OBJECTS-1) := (others => 0);
-    signal muon_pt_vector_bx_0: diff_inputs_array(0 to NR_MUON_OBJECTS-1) := (others => (others => '0'));
-    signal muon_eta_integer_bx_0: diff_integer_inputs_array(0 to NR_MUON_OBJECTS-1) := (others => 0);
-    signal muon_phi_integer_bx_0: diff_integer_inputs_array(0 to NR_MUON_OBJECTS-1) := (others => 0);
-    signal muon_cos_phi_bx_0: sin_cos_integer_array(0 to NR_MUON_OBJECTS-1) := (others => 0);
-    signal muon_sin_phi_bx_0: sin_cos_integer_array(0 to NR_MUON_OBJECTS-1) := (others => 0);
-    signal muon_eta_conv_2_muon_eta_integer_bx_0: diff_integer_inputs_array(0 to NR_MUON_OBJECTS-1) := (others => 0);
-    signal muon_phi_conv_2_muon_phi_integer_bx_0: diff_integer_inputs_array(0 to NR_MUON_OBJECTS-1) := (others => 0);
+    signal muon_pt_integer_bx_0: diff_integer_inputs_array(0 to NR_MU_OBJECTS-1) := (others => 0);
+    signal muon_pt_vector_bx_0: diff_inputs_array(0 to NR_MU_OBJECTS-1) := (others => (others => '0'));
+    signal muon_eta_integer_bx_0: diff_integer_inputs_array(0 to NR_MU_OBJECTS-1) := (others => 0);
+    signal muon_phi_integer_bx_0: diff_integer_inputs_array(0 to NR_MU_OBJECTS-1) := (others => 0);
+    signal mu_cos_phi_bx_0: sin_cos_integer_array(0 to NR_MU_OBJECTS-1) := (others => 0);
+    signal mu_sin_phi_bx_0: sin_cos_integer_array(0 to NR_MU_OBJECTS-1) := (others => 0);
 
 -- Signal definition of differences for correlation conditions.
 -- Insert "signal_correlation_conditions_differences.vhd.j2" once for correlation conditions of different ObjectTypes and Bx combinations.
-    signal muon_muon_bx_0_bx_0_diff_eta_integer: dim2_max_eta_range_array(0 to NR_MUON_OBJECTS-1, 0 to NR_MUON_OBJECTS-1) := (others => (others => 0));
-    signal muon_muon_bx_0_bx_0_diff_eta_integer_lut: diff_2dim_integer_array(0 to NR_MUON_OBJECTS-1, 0 to NR_MUON_OBJECTS-1) := (others => (others => 0));
-    signal muon_muon_bx_0_bx_0_diff_eta_vector: deta_dphi_vector_array(0 to NR_MUON_OBJECTS-1, 0 to NR_MUON_OBJECTS-1) := (others => (others => (others => '0')));
-    signal muon_muon_bx_0_bx_0_diff_phi_integer: dim2_max_phi_range_array(0 to NR_MUON_OBJECTS-1, 0 to NR_MUON_OBJECTS-1) := (others => (others => 0));
-    signal muon_muon_bx_0_bx_0_diff_phi_integer_lut: diff_2dim_integer_array(0 to NR_MUON_OBJECTS-1, 0 to NR_MUON_OBJECTS-1) := (others => (others => 0));
-    signal muon_muon_bx_0_bx_0_diff_phi_vector: deta_dphi_vector_array(0 to NR_MUON_OBJECTS-1, 0 to NR_MUON_OBJECTS-1) := (others => (others => (others => '0')));
-    signal muon_muon_bx_0_bx_0_cosh_deta_integer : diff_2dim_integer_array(0 to NR_MUON_OBJECTS-1, 0 to NR_MUON_OBJECTS-1) := (others => (others => 0));
-    signal muon_muon_bx_0_bx_0_cosh_deta_vector : muon_cosh_cos_vector_array(0 to NR_MUON_OBJECTS-1, 0 to NR_MUON_OBJECTS-1) := (others => (others => (others => '0')));
-    signal muon_muon_bx_0_bx_0_cos_dphi_integer : diff_2dim_integer_array(0 to NR_MUON_OBJECTS-1, 0 to NR_MUON_OBJECTS-1) := (others => (others => 0));
-    signal muon_muon_bx_0_bx_0_cos_dphi_vector : muon_cosh_cos_vector_array(0 to NR_MUON_OBJECTS-1, 0 to NR_MUON_OBJECTS-1) := (others => (others => (others => '0')));
+    signal mu_mu_bx_0_bx_0_diff_eta_integer: dim2_max_eta_range_array(0 to NR_MU_OBJECTS-1, 0 to NR_MU_OBJECTS-1) := (others => (others => 0));
+    signal mu_mu_bx_0_bx_0_diff_eta_integer_lut: diff_2dim_integer_array(0 to NR_MU_OBJECTS-1, 0 to NR_MU_OBJECTS-1) := (others => (others => 0));
+    signal mu_mu_bx_0_bx_0_diff_eta_vector: deta_dphi_vector_array(0 to NR_MU_OBJECTS-1, 0 to NR_MU_OBJECTS-1) := (others => (others => (others => '0')));
+    signal mu_mu_bx_0_bx_0_diff_phi_integer: dim2_max_phi_range_array(0 to NR_MU_OBJECTS-1, 0 to NR_MU_OBJECTS-1) := (others => (others => 0));
+    signal mu_mu_bx_0_bx_0_diff_phi_integer_lut: diff_2dim_integer_array(0 to NR_MU_OBJECTS-1, 0 to NR_MU_OBJECTS-1) := (others => (others => 0));
+    signal mu_mu_bx_0_bx_0_diff_phi_vector: deta_dphi_vector_array(0 to NR_MU_OBJECTS-1, 0 to NR_MU_OBJECTS-1) := (others => (others => (others => '0')));
+    signal mu_mu_bx_0_bx_0_cosh_deta_integer : diff_2dim_integer_array(0 to NR_MU_OBJECTS-1, 0 to NR_MU_OBJECTS-1) := (others => (others => 0));
+    signal mu_mu_bx_0_bx_0_cosh_deta_vector : muon_cosh_cos_vector_array(0 to NR_MU_OBJECTS-1, 0 to NR_MU_OBJECTS-1) := (others => (others => (others => '0')));
+    signal mu_mu_bx_0_bx_0_cos_dphi_integer : diff_2dim_integer_array(0 to NR_MU_OBJECTS-1, 0 to NR_MU_OBJECTS-1) := (others => (others => 0));
+    signal mu_mu_bx_0_bx_0_cos_dphi_vector : muon_cosh_cos_vector_array(0 to NR_MU_OBJECTS-1, 0 to NR_MU_OBJECTS-1) := (others => (others => (others => '0')));
 
-    signal muon_muon_bx_0_bx_0_deta_bin_vector : muon_deta_bin_vector_array(0 to NR_MUON_OBJECTS-1, 0 to NR_MUON_OBJECTS-1) := (others => (others => (others => '0')));
-    signal muon_muon_bx_0_bx_0_dphi_bin_vector : muon_dphi_bin_vector_array(0 to NR_MUON_OBJECTS-1, 0 to NR_MUON_OBJECTS-1) := (others => (others => (others => '0')));
+    signal mu_mu_bx_0_bx_0_deta_bin_vector : muon_deta_bin_vector_array(0 to NR_MU_OBJECTS-1, 0 to NR_MU_OBJECTS-1) := (others => (others => (others => '0')));
+    signal mu_mu_bx_0_bx_0_dphi_bin_vector : muon_dphi_bin_vector_array(0 to NR_MU_OBJECTS-1, 0 to NR_MU_OBJECTS-1) := (others => (others => (others => '0')));
     
 -- Signal definition for muon charge correlations.
 -- Insert "signal_muon_charge_correlations.vhd.j2" only once for a certain Bx combination,
