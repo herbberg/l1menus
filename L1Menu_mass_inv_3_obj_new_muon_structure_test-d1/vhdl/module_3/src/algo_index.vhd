61fb1feffea3203fe92bd72552a85778fb9dd9e3
herbberg <herbert.bergauer@oeaw.ac.at>
2020-06-22 15:25:09 +0200

    added new menu for tests

---
+++ L1Menu_mass_inv_3_obj_new_muon_structure_test-d1/vhdl/module_3/src/algo_index.vhd
@@ -0,0 +1,41 @@
+-- ========================================================
+-- from VHDL producer:
+
+-- Module ID: 3
+
+-- Name of L1 Trigger Menu:
+-- L1Menu_mass_inv_3_obj_new_muon_structure_test
+
+-- Unique ID of L1 Trigger Menu:
+-- d67a467b-c934-44b6-80b5-bf2f788eb265
+
+-- Unique ID of firmware implementation:
+-- 6b8f0854-8ece-4226-8e5d-96e5ea9b4d6f
+
+-- Scale set:
+-- scales_2020_06_16
+
+-- VHDL producer version
+-- v2.8.0
+
+-- HB 2016-09-16: constants for algo_mapping_rop.
+type global_index_array is array (0 to NR_ALGOS-1) of integer;
+constant global_index: global_index_array := (
+        425, -- module_index: 0, name: L1_ETMHF140
+        398, -- module_index: 1, name: L1_HTT120er
+        405, -- module_index: 2, name: L1_HTT400er
+        166, -- module_index: 3, name: L1_SingleEG28er1p5
+        168, -- module_index: 4, name: L1_SingleEG36er2p5
+        172, -- module_index: 5, name: L1_SingleEG45er2p5
+        184, -- module_index: 6, name: L1_SingleIsoEG24er1p5
+        185, -- module_index: 7, name: L1_SingleIsoEG26er2p5
+        192, -- module_index: 8, name: L1_SingleIsoEG30er2p5
+        312, -- module_index: 9, name: L1_SingleJet120
+        361, -- module_index: 10, name: L1_DoubleJet_120_45_DoubleJet45_Mass_Min620_Jet60TT28
+        360, -- module_index: 11, name: L1_DoubleJet_115_40_DoubleJet40_Mass_Min620_Jet60TT28
+        134, -- module_index: 12, name: L1_Mu10er2p3_Jet32er2p3_dR_Max0p4_DoubleJet32er2p3_dEta_Max1p6
+        364, -- module_index: 13, name: L1_DoubleJet_80_30_Mass_Min420_IsoTau40_RmOvlp
+    others => 0
+);
+
+-- ========================================================
