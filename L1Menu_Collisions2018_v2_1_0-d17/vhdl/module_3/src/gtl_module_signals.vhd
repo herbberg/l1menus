0eb52164bdc779cd6eff06393ec1877e0f73bdc6
Herbert Bergauer <herbert.bergauer@oeaw.ac.at>
2020-08-11 11:27:11 +0200

    added new distribution

---
+++ L1Menu_Collisions2018_v2_1_0-d17/vhdl/module_3/src/gtl_module_signals.vhd
@@ -0,0 +1,106 @@
+-- ========================================================
+-- from VHDL producer:
+
+-- Module ID: 3
+
+-- Name of L1 Trigger Menu:
+-- L1Menu_Collisions2018_v2_1_0
+
+-- Unique ID of L1 Trigger Menu:
+-- 8ebe92b7-28fd-4707-8bfb-ee7113a44883
+
+-- Unique ID of firmware implementation:
+-- 0b48b914-f231-4de0-8f57-fedab5083f9b
+
+-- Scale set:
+-- scales_2020_06_16
+
+-- VHDL producer version
+-- v2.9.0
+
+-- Signal definition of pt, eta and phi for correlation conditions.
+-- Insert "signal_correlation_conditions_pt_eta_phi_cos_sin_phi.vhd.j2" as often as an ObjectType at a certain Bx is used in a correlation condition.
+    signal jet_pt_vector_bx_0: diff_inputs_array(0 to NR_JET_OBJECTS-1) := (others => (others => '0'));
+    signal jet_eta_integer_bx_0: diff_integer_inputs_array(0 to NR_JET_OBJECTS-1) := (others => 0);
+    signal jet_phi_integer_bx_0: diff_integer_inputs_array(0 to NR_JET_OBJECTS-1) := (others => 0);
+    signal jet_cos_phi_bx_0: sin_cos_integer_array(0 to NR_JET_OBJECTS-1) := (others => 0);
+    signal jet_sin_phi_bx_0: sin_cos_integer_array(0 to NR_JET_OBJECTS-1) := (others => 0);
+    signal conv_jet_cos_phi_bx_0: sin_cos_integer_array(0 to NR_JET_OBJECTS-1) := (others => 0);
+    signal conv_jet_sin_phi_bx_0: sin_cos_integer_array(0 to NR_JET_OBJECTS-1) := (others => 0);
+    signal jet_eta_conv_2_muon_eta_integer_bx_0: diff_integer_inputs_array(0 to NR_JET_OBJECTS-1) := (others => 0);
+    signal jet_phi_conv_2_muon_phi_integer_bx_0: diff_integer_inputs_array(0 to NR_JET_OBJECTS-1) := (others => 0);
+    signal mu_pt_vector_bx_0: diff_inputs_array(0 to NR_MU_OBJECTS-1) := (others => (others => '0'));
+    signal mu_upt_vector_bx_0: diff_inputs_array(0 to NR_MU_OBJECTS-1) := (others => (others => '0'));
+    signal mu_eta_integer_bx_0: diff_integer_inputs_array(0 to NR_MU_OBJECTS-1) := (others => 0);
+    signal mu_phi_integer_bx_0: diff_integer_inputs_array(0 to NR_MU_OBJECTS-1) := (others => 0);
+    signal mu_cos_phi_bx_0: sin_cos_integer_array(0 to NR_MU_OBJECTS-1) := (others => 0);
+    signal mu_sin_phi_bx_0: sin_cos_integer_array(0 to NR_MU_OBJECTS-1) := (others => 0);
+
+-- Signal definition of differences for correlation conditions.
+-- Insert "signal_correlation_conditions_differences.vhd.j2" once for correlation conditions of different ObjectTypes and Bx combinations.
+    signal jet_jet_bx_0_bx_0_deta_integer: dim2_max_eta_range_array(0 to NR_JET_OBJECTS-1, 0 to NR_JET_OBJECTS-1) := (others => (others => 0));
+    signal jet_jet_bx_0_bx_0_deta_vector: deta_dphi_vector_array(0 to NR_JET_OBJECTS-1, 0 to NR_JET_OBJECTS-1) := (others => (others => (others => '0')));
+    signal jet_jet_bx_0_bx_0_dphi_integer: dim2_max_phi_range_array(0 to NR_JET_OBJECTS-1, 0 to NR_JET_OBJECTS-1) := (others => (others => 0));
+    signal jet_jet_bx_0_bx_0_dphi_vector: deta_dphi_vector_array(0 to NR_JET_OBJECTS-1, 0 to NR_JET_OBJECTS-1) := (others => (others => (others => '0')));
+    signal jet_jet_bx_0_bx_0_deta_bin_vector : calo_deta_bin_vector_array(0 to NR_JET_OBJECTS-1, 0 to NR_JET_OBJECTS-1) := (others => (others => (others => '0')));
+    signal jet_jet_bx_0_bx_0_dphi_bin_vector : calo_dphi_bin_vector_array(0 to NR_JET_OBJECTS-1, 0 to NR_JET_OBJECTS-1) := (others => (others => (others => '0')));
+    signal jet_mu_bx_0_bx_0_deta_integer: dim2_max_eta_range_array(0 to NR_JET_OBJECTS-1, 0 to NR_MU_OBJECTS-1) := (others => (others => 0));
+    signal jet_mu_bx_0_bx_0_deta_vector: deta_dphi_vector_array(0 to NR_JET_OBJECTS-1, 0 to NR_MU_OBJECTS-1) := (others => (others => (others => '0')));
+    signal jet_mu_bx_0_bx_0_dphi_integer: dim2_max_phi_range_array(0 to NR_JET_OBJECTS-1, 0 to NR_MU_OBJECTS-1) := (others => (others => 0));
+    signal jet_mu_bx_0_bx_0_dphi_vector: deta_dphi_vector_array(0 to NR_JET_OBJECTS-1, 0 to NR_MU_OBJECTS-1) := (others => (others => (others => '0')));
+    signal jet_mu_bx_0_bx_0_deta_bin_vector : calo_deta_bin_vector_array(0 to NR_JET_OBJECTS-1, 0 to NR_MU_OBJECTS-1) := (others => (others => (others => '0')));
+    signal jet_mu_bx_0_bx_0_dphi_bin_vector : calo_dphi_bin_vector_array(0 to NR_JET_OBJECTS-1, 0 to NR_MU_OBJECTS-1) := (others => (others => (others => '0')));
+    signal jet_jet_bx_0_bx_0_cosh_deta_vector : calo_cosh_cos_vector_array(0 to NR_JET_OBJECTS-1, 0 to NR_JET_OBJECTS-1) := (others => (others => (others => '0')));
+    signal jet_jet_bx_0_bx_0_cos_dphi_vector : calo_cosh_cos_vector_array(0 to NR_JET_OBJECTS-1, 0 to NR_JET_OBJECTS-1) := (others => (others => (others => '0')));
+    signal jet_mu_bx_0_bx_0_delta_r_vector: delta_r_vector_array(0 to NR_JET_OBJECTS-1, 0 to NR_MU_OBJECTS-1) := (others => (others => (others => '0')));
+    signal jet_jet_bx_0_bx_0_mass_inv_vector : mass_vector_array(0 to NR_JET_OBJECTS-1, 0 to NR_JET_OBJECTS-1) := (others => (others => (others => '0')));
+
+-- Signal definition for muon charge correlations.
+-- Insert "signal_muon_charge_correlations.vhd.j2" only once for a certain Bx combination,
+-- if there is at least one muon condition or one muon-muon correlation condition.
+    signal ls_charcorr_double_bx_0_bx_0, os_charcorr_double_bx_0_bx_0 : muon_charcorr_double_array;
+    signal ls_charcorr_triple_bx_0_bx_0, os_charcorr_triple_bx_0_bx_0 : muon_charcorr_triple_array;
+    signal ls_charcorr_quad_bx_0_bx_0, os_charcorr_quad_bx_0_bx_0 : muon_charcorr_quad_array;
+
+-- Signal definition for conditions names
+    signal single_etmhf_i318 : std_logic;
+    signal single_htt_i304 : std_logic;
+    signal single_htt_i309 : std_logic;
+    signal calo_calo_correlation_i114 : std_logic;
+    signal calo_muon_correlation_i113 : std_logic;
+    signal invariant_mass_i275 : std_logic;
+    signal invariant_mass_i276 : std_logic;
+    signal invariant_mass_i277 : std_logic;
+    signal invariant_mass_i278 : std_logic;
+    signal invariant_mass_i279 : std_logic;
+    signal invariant_mass_i280 : std_logic;
+    signal invariant_mass_i281 : std_logic;
+    signal invariant_mass_i282 : std_logic;
+    signal invariant_mass_i283 : std_logic;
+    signal invariant_mass_i284 : std_logic;
+    signal invariant_mass_i285 : std_logic;
+    signal single_eg_i133 : std_logic;
+    signal single_eg_i135 : std_logic;
+    signal single_eg_i139 : std_logic;
+    signal single_eg_i152 : std_logic;
+    signal single_eg_i153 : std_logic;
+    signal single_eg_i161 : std_logic;
+    signal single_jet_i233 : std_logic;
+    signal single_jet_i274 : std_logic;
+
+-- Signal definition for algorithms names
+    signal l1_mu12er2p3_jet40er2p1_d_r_max0p4_double_jet40er2p1_d_eta_max1p6 : std_logic;
+    signal l1_single_eg28er1p5 : std_logic;
+    signal l1_single_eg36er2p5 : std_logic;
+    signal l1_single_eg45er2p5 : std_logic;
+    signal l1_single_iso_eg24er1p5 : std_logic;
+    signal l1_single_iso_eg26er2p5 : std_logic;
+    signal l1_single_iso_eg30er2p5 : std_logic;
+    signal l1_single_jet120 : std_logic;
+    signal l1_double_jet_115_40_double_jet40_mass_min620_jet60_tt28 : std_logic;
+    signal l1_double_jet_120_45_double_jet45_mass_min620_jet60_tt28 : std_logic;
+    signal l1_htt120er : std_logic;
+    signal l1_htt400er : std_logic;
+    signal l1_etmhf140 : std_logic;
+
+-- ========================================================
