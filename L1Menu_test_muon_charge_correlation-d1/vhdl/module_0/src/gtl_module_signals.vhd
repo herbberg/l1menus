7d49f1e7382e5e3d03e5f79c85097dd5d6cef459
Herbert Bergauer <herbert.bergauer@oeaw.ac.at>
2020-08-14 09:20:49 +0200

    added new menu

---
+++ L1Menu_test_muon_charge_correlation-d1/vhdl/module_0/src/gtl_module_signals.vhd
@@ -0,0 +1,40 @@
+-- ========================================================
+-- from VHDL producer:
+
+-- Module ID: 0
+
+-- Name of L1 Trigger Menu:
+-- L1Menu_test_muon_charge_correlation
+
+-- Unique ID of L1 Trigger Menu:
+-- 1ae9dee9-6017-461b-a3e4-a221e19d28a2
+
+-- Unique ID of firmware implementation:
+-- 77e96942-b281-4e45-9dfc-1f81a65c4548
+
+-- Scale set:
+-- scales_2018_08_07
+
+-- VHDL producer version
+-- v2.7.5
+
+-- Signal definition of pt, eta and phi for correlation conditions.
+-- Insert "signal_correlation_conditions_pt_eta_phi_cos_sin_phi.vhd.j2" as often as an ObjectType at a certain Bx is used in a correlation condition.
+
+-- Signal definition of differences for correlation conditions.
+-- Insert "signal_correlation_conditions_differences.vhd.j2" once for correlation conditions of different ObjectTypes and Bx combinations.
+
+-- Signal definition for muon charge correlations.
+-- Insert "signal_muon_charge_correlations.vhd.j2" only once for a certain Bx combination,
+-- if there is at least one muon condition or one muon-muon correlation condition.
+    signal ls_charcorr_double_bx_0_bx_0, os_charcorr_double_bx_0_bx_0 : muon_charcorr_double_array;
+    signal ls_charcorr_triple_bx_0_bx_0, os_charcorr_triple_bx_0_bx_0 : muon_charcorr_triple_array;
+    signal ls_charcorr_quad_bx_0_bx_0, os_charcorr_quad_bx_0_bx_0 : muon_charcorr_quad_array;
+
+-- Signal definition for conditions names
+    signal double_mu_i5 : std_logic;
+
+-- Signal definition for algorithms names
+    signal l1_double_mu10_chgcor_ls : std_logic;
+
+-- ========================================================
