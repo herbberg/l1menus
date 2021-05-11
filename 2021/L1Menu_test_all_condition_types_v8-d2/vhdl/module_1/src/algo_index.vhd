59d0f8a36839ccbeef9a0bf018e3fd229f489ad1
herbberg <Herbert.Bergauer@oeaw.ac.at>
2021-05-11 09:17:55 +0200

    added new distribution

---
+++ 2021/L1Menu_test_all_condition_types_v8-d2/vhdl/module_1/src/algo_index.vhd
@@ -0,0 +1,32 @@
+-- ========================================================
+-- from VHDL producer:
+
+-- Module ID: 1
+
+-- Name of L1 Trigger Menu:
+-- L1Menu_test_all_condition_types_v8
+
+-- Unique ID of L1 Trigger Menu:
+-- 80b970ab-e353-4939-a077-e24cf9674d78
+
+-- Unique ID of firmware implementation:
+-- 7105973a-ae7b-4ec3-a138-4951e2dc050b
+
+-- Scale set:
+-- scales_2021_03_02
+
+-- VHDL producer version
+-- v2.11.0
+
+-- tmEventSetup version
+-- v0.8.1
+
+-- HB 2016-09-16: constants for algo_mapping_rop.
+type global_index_array is array (0 to NR_ALGOS-1) of integer;
+constant global_index: global_index_array := (
+          1, -- module_index: 0, name: L1_DoubleMu10_TBPT_min_20_CHGCOR_os
+         10, -- module_index: 1, name: L1_DoubleEg10_Jet10orm_MASS_MIN_40_TBPT_min_20_ORMDR_1
+    others => 0
+);
+
+-- ========================================================
---
+++ 2021/L1Menu_test_all_condition_types_v8-d2/vhdl/module_3/src/algo_index.vhd
@@ -0,0 +1,33 @@
+-- ========================================================
+-- from VHDL producer:
+
+-- Module ID: 3
+
+-- Name of L1 Trigger Menu:
+-- L1Menu_test_all_condition_types_v8
+
+-- Unique ID of L1 Trigger Menu:
+-- 80b970ab-e353-4939-a077-e24cf9674d78
+
+-- Unique ID of firmware implementation:
+-- 7105973a-ae7b-4ec3-a138-4951e2dc050b
+
+-- Scale set:
+-- scales_2021_03_02
+
+-- VHDL producer version
+-- v2.11.0
+
+-- tmEventSetup version
+-- v0.8.1
+
+-- HB 2016-09-16: constants for algo_mapping_rop.
+type global_index_array is array (0 to NR_ALGOS-1) of integer;
+constant global_index: global_index_array := (
+          8, -- module_index: 0, name: L1_DoubleEg10_Jet10orm_DETA_MAX_1p6_ORMDPHI_1
+          3, -- module_index: 1, name: L1_QuadMu10_CHGCOR_os
+         14, -- module_index: 2, name: L1_DoubleMu10_MASSDR_40
+    others => 0
+);
+
+-- ========================================================
