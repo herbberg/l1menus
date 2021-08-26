b7d140bc7beeb35e4dcfaa7c84f57a3ab0edf8d7
herbberg <herbert.bergauer@oeaw.ac.at>
2021-08-26 13:39:11 +0200

    typo

--- 2021/L1Menu_test_rom-d1/vhdl/module_0/src/gtl_module_instances.vhd
+++ 2021/L1Menu_test_rom-d1/vhdl/module_0/src/gtl_module_instances.vhd
@@ -1,7 +1,7 @@
 -- ========================================================
 -- Instantiations of conditions
 --
-addr_lsb <= bx_data.eg(2)(0)(16 downto 9) & bx_data.eg(2)(0)(24 downto 17);
+addr_lsb <= bx_data.eg(2)(0)(14 downto 9) & bx_data.eg(2)(0)(22 downto 17);
 
 lut1_i : entity work.rom_lut_calo_inv_dr_sq_1
     port map (
@@ -13,7 +13,7 @@ lut1_i : entity work.rom_lut_calo_inv_dr_sq_1
 -- ========================================================
 -- Instantiations of algorithms
 
-l1_dummy <= '1' when dout > 100000 else ’0’;
+l1_dummy <= '1' when dout > 100000 else '0';
 
 algo(0) <= l1_dummy;
 
