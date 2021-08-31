cf7964c01f7cbec4463265478ac72f68becadb4b
herbberg <herbert.bergauer@oeaw.ac.at>
2021-08-31 09:40:44 +0200

    deleted obsolete test menus

--- 2021/L1Menu_test_rom-d2/vhdl/module_0/src/algo_index.vhd
+++
@@ -1,7 +0,0 @@
-type global_index_array is array (0 to NR_ALGOS-1) of integer;
-constant global_index: global_index_array := (
-          0, -- module_index: 0, name: L1_dummy
-    others => 0
-);
-
--- ========================================================
--- 2021/L1Menu_test_rom-d3/vhdl/module_0/src/gtl_module_signals.vhd
+++
@@ -1,7 +0,0 @@
-    signal dout : std_logic_vector(25 downto 0) := (others => '0');
-    signal addr_lsb : std_logic_vector(11 DOWNTO 0) := (others => '0');
-
--- Signal definition for algorithms names
-    signal l1_dummy : std_logic := '0';
-
--- ========================================================
