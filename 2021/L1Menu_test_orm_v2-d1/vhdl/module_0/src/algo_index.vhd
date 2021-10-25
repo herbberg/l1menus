0c165df7cd56f81563c3979f55c079909f08563b
herbberg <herbert.bergauer@oeaw.ac.at>
2021-10-25 11:49:11 +0200

    added L1Menu_test_orm_v2-d1 for tests

---
+++ 2021/L1Menu_test_orm_v2-d1/vhdl/module_0/src/algo_index.vhd
@@ -0,0 +1,31 @@
+-- ========================================================
+-- from VHDL producer:
+
+-- Module ID: 0
+
+-- Name of L1 Trigger Menu:
+-- L1Menu_test_orm_v2
+
+-- Unique ID of L1 Trigger Menu:
+-- fc7317f7-50f3-44e3-a476-844329774ca5
+
+-- Unique ID of firmware implementation:
+-- 242582e2-1420-4ef3-bbc3-20624f2dd736
+
+-- Scale set:
+-- scales_2021_03_02
+
+-- VHDL producer version
+-- v2.11.1
+
+-- tmEventSetup version
+-- v0.9.1
+
+-- HB 2016-09-16: constants for algo_mapping_rop.
+type global_index_array is array (0 to NR_ALGOS-1) of integer;
+constant global_index: global_index_array := (
+        363, -- module_index: 0, name: L1_DoubleJet35_Mass_Min450_IsoTau45_RmOvlp
+    others => 0
+);
+
+-- ========================================================
