24d7d4822c2afe1e1bf499dd07ae2a522320c21a
herbberg <herbert.bergauer@oeaw.ac.at>
2021-10-27 10:04:55 +0200

    added comments

--- scripts/calc_tv_val.py
+++ scripts/calc_tv_val.py
@@ -8,14 +8,14 @@ import math
 import os
 import sys
 
-# step width
-hex_char='%08x'
+# eta and phi definitions
 eta_bit_width=8
 phi_bins=144
 eta_step=0.087/2
 phi_step=2*math.pi/phi_bins
 
-# values for bit shift inside of 32 bit structure
+# values for bit shift inside of 32 bit structure (defaults for 'eg' and 'tau'
+hex_str='%08x'
 shift_eta=9
 shift_phi=17
 shift_iso=25
@@ -30,6 +30,7 @@ phi=float(sys.argv[3])
 iso=int(sys.argv[4])
 
 pt_idx=int(pt*2)
+
 eta_idx=int(eta/eta_step)
 if eta < 0:
     eta_idx=2**eta_bit_width-abs(eta_idx)
@@ -48,6 +49,5 @@ if sys.argv[5] == 'jet':
 else:
     val=iso_sh|phi_sh|eta_sh|pt_idx
 
-#print(hex(val))
-print(hex_char % val)
+print(hex_str % val)
 
