25468f5e42c9d0da13e1c6546135f625d638b5f3
herbberg <herbert.bergauer@oeaw.ac.at>
2022-01-21 09:50:56 +0100

    added new menu

---
+++ 2022/L1Menu_Collisions2022_adt_test_ext_sig-d1/vhdl/module_0/src/algo_index.vhd
@@ -0,0 +1,31 @@
+-- ========================================================
+-- from VHDL producer:
+
+-- Module ID: 0
+
+-- Name of L1 Trigger Menu:
+-- L1Menu_Collisions2022_adt_test_ext_sig
+
+-- Unique ID of L1 Trigger Menu:
+-- a0494794-5846-41a9-b875-ed3533a1c0e6
+
+-- Unique ID of firmware implementation:
+-- 4f5ec419-fbb3-4057-b750-474db63f52b3
+
+-- Scale set:
+-- scales_2021_03_02
+
+-- VHDL producer version
+-- v2.12.0
+
+-- tmEventSetup version
+-- v0.10.0
+
+-- HB 2016-09-16: constants for algo_mapping_rop.
+type global_index_array is array (0 to NR_ALGOS-1) of integer;
+constant global_index: global_index_array := (
+          0, -- module_index: 0, name: L1_ADT_0
+    others => 0
+);
+
+-- ========================================================
