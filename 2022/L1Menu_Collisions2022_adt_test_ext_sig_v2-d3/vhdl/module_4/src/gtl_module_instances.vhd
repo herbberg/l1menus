ba2121b0d0c2e3f157eb92b060a1afaf55cf0d01
herbberg <herbert.bergauer@oeaw.ac.at>
2022-01-24 10:27:34 +0100

    added new distribution

---
+++ 2022/L1Menu_Collisions2022_adt_test_ext_sig_v2-d3/vhdl/module_4/src/gtl_module_instances.vhd
@@ -0,0 +1,82 @@
+-- ========================================================
+-- from VHDL producer:
+
+-- Module ID: 4
+
+-- Name of L1 Trigger Menu:
+-- L1Menu_Collisions2022_adt_test_ext_sig_v2
+
+-- Unique ID of L1 Trigger Menu:
+-- b581e141-1a2d-45fa-81d7-45348fab94a3
+
+-- Unique ID of firmware implementation:
+-- e8c3c770-2d65-44e6-bcd7-da256a4cb74f
+
+-- Scale set:
+-- scales_2021_03_02
+
+-- VHDL producer version
+-- v2.14.0
+
+-- tmEventSetup version
+-- v0.10.0
+
+-- ========================================================
+-- Instantiations of conditions
+--
+
+cond_adt_4_i: entity work.adt_4_dummy
+    port map(
+        lhc_clk,
+        bx_data(2).muon,
+        bx_data(2).eg,
+        bx_data(2).jet,
+        bx_data(2).tau,
+        bx_data(2).ett,
+        bx_data(2).htt,
+        bx_data(2).etm,
+        bx_data(2).htm,
+        bx_data(2).ettem,
+        bx_data(2).etmhf,
+        single_ext_i4
+    );
+
+-- ========================================================
+-- Instantiations of algorithms
+
+-- 4 L1_ADT_4 : EXT_ADT_4
+l1_adt_4 <= single_ext_i4;
+algo(0) <= l1_adt_4;
+
+-- ========================================================
+-- Instantiations conversions, calculations, etc.
+-- eta and phi conversion to muon scale for calo-muon and muon-esums correlation conditions (used for DETA, DPHI, DR and mass)
+
+-- pt, eta, phi, cosine phi and sine phi for correlation conditions (used for DETA, DPHI, DR, mass, overlap_remover and two-body pt)
+
+-- deta and dphi calculations for correlation conditions (used for DETA, DPHI)
+
+-- eta, dphi, cosh deta and cos dphi LUTs for correlation conditions (used for DR and mass)
+--
+-- Instantiations of correlation cuts calculations
+--
+-- Instantiations of DeltaEta LUTs
+
+-- Instantiations of DeltaPhi LUTs
+
+-- Instantiations of DeltaR calculation
+
+-- Instantiations of Invariant mass calculation
+
+-- Instantiations of Invariant mass divided DeltaR calculation
+
+-- Instantiations of Invariant mass unconstrained pt calculation
+
+-- Instantiations of Transverse mass calculation
+
+-- Instantiations of Two-body pt calculation
+
+-- muon charge correlations
+
+
+-- ========================================================
---
+++ 2022/L1Menu_Collisions2022_adt_test_ext_sig_v2-d3/vhdl/module_5/src/algo_index.vhd
@@ -0,0 +1,31 @@
+-- ========================================================
+-- from VHDL producer:
+
+-- Module ID: 5
+
+-- Name of L1 Trigger Menu:
+-- L1Menu_Collisions2022_adt_test_ext_sig_v2
+
+-- Unique ID of L1 Trigger Menu:
+-- b581e141-1a2d-45fa-81d7-45348fab94a3
+
+-- Unique ID of firmware implementation:
+-- e8c3c770-2d65-44e6-bcd7-da256a4cb74f
+
+-- Scale set:
+-- scales_2021_03_02
+
+-- VHDL producer version
+-- v2.14.0
+
+-- tmEventSetup version
+-- v0.10.0
+
+-- HB 2016-09-16: constants for algo_mapping_rop.
+type global_index_array is array (0 to NR_ALGOS-1) of integer;
+constant global_index: global_index_array := (
+          5, -- module_index: 0, name: L1_ADT_5
+    others => 0
+);
+
+-- ========================================================
