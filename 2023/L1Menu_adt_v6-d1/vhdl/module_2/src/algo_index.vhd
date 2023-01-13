154abdb7c15bbe265e20c98981417b849a28be45
hbergaue <herbert.bergauer@oeaw.ac.at>
2023-01-13 11:21:19 +0100

    added new directory and menu

---
+++ 2023/L1Menu_adt_v6-d1/vhdl/module_2/src/algo_index.vhd
@@ -0,0 +1,31 @@
+-- ========================================================
+-- from VHDL producer:
+
+-- Module ID: 2
+
+-- Name of L1 Trigger Menu:
+-- L1Menu_adt_v6
+
+-- Unique ID of L1 Trigger Menu:
+-- f91c4212-b199-4c73-a521-37070035039a
+
+-- Unique ID of firmware implementation:
+-- 650d621a-0abe-4280-9b34-5f75d23321aa
+
+-- Scale set:
+-- scales_2021_03_02
+
+-- VHDL producer version
+-- v2.14.0
+
+-- tmEventSetup version
+-- v0.10.0
+
+-- HB 2016-09-16: constants for algo_mapping_rop.
+type global_index_array is array (0 to NR_ALGOS-1) of integer;
+constant global_index: global_index_array := (
+          1, -- module_index: 0, name: L1_ADT_4000
+    others => 0
+);
+
+-- ========================================================
---
+++ 2023/L1Menu_adt_v6-d1/vhdl/module_4/src/gtl_module_instances.vhd
@@ -0,0 +1,68 @@
+-- ========================================================
+-- from VHDL producer:
+
+-- Module ID: 4
+
+-- Name of L1 Trigger Menu:
+-- L1Menu_adt_v6
+
+-- Unique ID of L1 Trigger Menu:
+-- f91c4212-b199-4c73-a521-37070035039a
+
+-- Unique ID of firmware implementation:
+-- 650d621a-0abe-4280-9b34-5f75d23321aa
+
+-- Scale set:
+-- scales_2021_03_02
+
+-- VHDL producer version
+-- v2.14.0
+
+-- tmEventSetup version
+-- v0.10.0
+
+-- ========================================================
+-- Instantiations of conditions
+--
+-- External condition assignment
+single_ext_i4 <= bx_data.ext_cond(2)(40); -- EXT_TOTEM_1
+
+-- ========================================================
+-- Instantiations of algorithms
+
+-- 4 L1_dummy_1 : EXT_TOTEM_1
+l1_dummy_1 <= single_ext_i4;
+algo(0) <= l1_dummy_1;
+
+-- ========================================================
+-- Instantiations conversions, calculations, etc.
+-- eta and phi conversion to muon scale for calo-muon and muon-esums correlation conditions (used for DETA, DPHI, DR and mass)
+
+-- pt, eta, phi, cosine phi and sine phi for correlation conditions (used for DETA, DPHI, DR, mass, overlap_remover and two-body pt)
+
+-- deta and dphi calculations for correlation conditions (used for DETA, DPHI)
+
+-- eta, dphi, cosh deta and cos dphi LUTs for correlation conditions (used for DR and mass)
+--
+-- Instantiations of correlation cuts calculations
+--
+-- Instantiations of DeltaEta LUTs
+
+-- Instantiations of DeltaPhi LUTs
+
+-- Instantiations of DeltaR calculation
+
+-- Instantiations of Invariant mass calculation
+
+-- Instantiations of Invariant mass divided DeltaR calculation
+
+-- Instantiations of Invariant mass unconstrained pt calculation
+
+-- Instantiations of Transverse mass calculation
+
+-- Instantiations of Two-body pt calculation
+
+-- muon charge correlations
+
+
+-- ========================================================
