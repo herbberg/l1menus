be73a2cd845b8d9eee34e59c58c47423d1f1375f
hbergaue <herbert.bergauer@oeaw.ac.at>
2023-01-17 10:26:32 +0100

    fixed typo

--- 2023/L1Menu_adt_v6-d1/vhdl/module_2/src/gtl_module_instances.vhd
+++ 2023/L1Menu_adt_v6-d1/vhdl/module_2/src/gtl_module_instances.vhd
@@ -31,8 +31,8 @@ cond_ext_adt_4000_i: entity work.adt_wrapper
     port map(
         lhc_clk,
         bx_data.mu(2),
-        bx_data.jet(2),
         bx_data.eg(2),
+        bx_data.jet(2),
         bx_data.tau(2),
         bx_data.ett(2),
         bx_data.htt(2),
