7124c408a96018a6e3da05cbd2826c5687526f16
hbergaue <herbert.bergauer@oeaw.ac.at>
2023-01-30 08:31:43 +0100

    added new distribution

---
+++ 2023/L1Menu_adt_v6-d3/vhdl/module_4/src/gtl_module_instances.vhd
@@ -0,0 +1,69 @@
+-- ========================================================
+-- from VHDL producer:
+
+-- Module ID: 4
+
+-- Name of L1 Trigger Menu:
+-- L1Menu_adt_v6
+
+-- Unique ID of L1 Trigger Menu:
+-- f91c4212-b199-4c73-a521-37070035039a
+
+-- Unique ID of firmware implementation:
+-- bd663e64-e3e4-446f-848a-e1cb813e6d8d
+
+-- Scale set:
+-- scales_2021_03_02
+
+-- VHDL producer version
+-- v2.14.0
+
+-- tmEventSetup version
+-- v0.10.0
+
+-- ========================================================
+-- Instantiations of conditions
+--
+-- External condition assignment
+single_ext_i4 <= bx_data.ext_cond(2)(40); -- EXT_TOTEM_1
+
+-- ========================================================
+-- Instantiations of algorithms
+
+-- 4 L1_dummy_1 : EXT_TOTEM_1
+l1_dummy_1 <= single_ext_i4;
+algo_0_p : entity work.reg
+    port map (lhc_clk, l1_dummy_1, algo_o(0));
+
+-- ========================================================
+-- Instantiations conversions, calculations, etc.
+-- eta and phi conversion to muon scale for calo-muon and muon-esums correlation conditions (used for DETA, DPHI, DR and mass)
+
+-- pt, eta, phi, cosine phi and sine phi for correlation conditions (used for DETA, DPHI, DR, mass, overlap_remover and two-body pt)
+
+-- deta and dphi calculations for correlation conditions (used for DETA, DPHI)
+
+-- eta, dphi, cosh deta and cos dphi LUTs for correlation conditions (used for DR and mass)
+--
+-- Instantiations of correlation cuts calculations
+--
+-- Instantiations of DeltaEta LUTs
+
+-- Instantiations of DeltaPhi LUTs
+
+-- Instantiations of DeltaR calculation
+
+-- Instantiations of Invariant mass calculation
+
+-- Instantiations of Invariant mass divided DeltaR calculation
+
+-- Instantiations of Invariant mass unconstrained pt calculation
+
+-- Instantiations of Transverse mass calculation
+
+-- Instantiations of Two-body pt calculation
+
+-- muon charge correlations
+
+
+-- ========================================================
