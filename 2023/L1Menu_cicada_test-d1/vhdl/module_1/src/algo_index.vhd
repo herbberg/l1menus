1b92f92e773ad2c8e2aed20b7394d8cbb633747a
hbergaue <herbert.bergauer@oeaw.ac.at>
2023-02-01 12:19:49 +0100

    added new menu for tests

---
+++ 2023/L1Menu_cicada_test-d1/vhdl/module_1/src/algo_index.vhd
@@ -0,0 +1,31 @@
+-- ========================================================
+-- from VHDL producer:
+
+-- Module ID: 1
+
+-- Name of L1 Trigger Menu:
+-- L1Menu_cicada_test
+
+-- Unique ID of L1 Trigger Menu:
+-- 60505f22-e28a-4665-b1ab-3248347defcb
+
+-- Unique ID of firmware implementation:
+-- bfef8aa9-e42b-4b0f-af95-c29f9411bc39
+
+-- Scale set:
+-- scales_2021_03_02
+
+-- VHDL producer version
+-- v2.14.0
+
+-- tmEventSetup version
+-- v0.10.0
+
+-- HB 2016-09-16: constants for algo_mapping_rop.
+type global_index_array is array (0 to NR_ALGOS-1) of integer;
+constant global_index: global_index_array := (
+          1, -- module_index: 0, name: L1_Double_Bjet_10_20
+    others => 0
+);
+
+-- ========================================================
