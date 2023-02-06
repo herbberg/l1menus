cee0e2affcf856d69178c0f216b49389274b6a60
hbergaue <herbert.bergauer@oeaw.ac.at>
2023-02-06 11:04:40 +0100

    added new menu, added READ.me files

---
+++ 2023/L1Menu_cicada_test_v2-d1/vhdl/module_5/src/gtl_module_instances.vhd
@@ -0,0 +1,93 @@
+-- ========================================================
+-- from VHDL producer:
+
+-- Module ID: 5
+
+-- Name of L1 Trigger Menu:
+-- L1Menu_cicada_test
+
+-- Unique ID of L1 Trigger Menu:
+-- 60505f22-e28a-4665-b1ab-3248347defcb
+
+-- Unique ID of firmware implementation:
+-- bfef8aa9-e42b-4b0f-af95-c29f9411bc39
+
+-- Scale set:
+-- scales_2021_03_02
+
+-- VHDL producer version
+-- v2.14.0
+
+-- tmEventSetup version
+-- v0.10.0
+
+-- ========================================================
+-- Instantiations of conditions
+--
+-- REMARK: manually added "Anomaly Detection" requirements for tests
+
+cond_single_bjet_i5_i: entity work.comb_conditions
+    generic map(
+-- setting slice high value(s) instead of default value(s) ("NR_MU_OBJECTS-1" => 7)
+        slice_1_high_obj1 => 5,
+        slice_2_high_obj1 => 5,
+        slice_3_high_obj1 => 5,
+        slice_4_high_obj1 => 5,
+-- object cuts
+        pt_thresholds_obj1 => (X"0064", X"0000", X"0000", X"0000"),
+-- number of objects and type
+        nr_obj1 => NR_BJET_OBJECTS,
+        type_obj1 => BJET_TYPE,
+        ad_requ  => true,
+        ad_dec_thr => "0010000",
+        ad_int_thr => "000010",
+        nr_templates => 1
+    )
+    port map(
+        lhc_clk,
+        obj1_calo => bx_data.bjet(2),
+        ad_dec_i => bx_data.ad(2)(AD_DEC_HIGH downto AD_DEC_LOW),
+        ad_int_i => bx_data.ad(2)(AD_INT_HIGH downto AD_INT_LOW),
+        condition_o => single_bjet_i5
+    );
+
+
+-- ========================================================
+-- Instantiations of algorithms
+
+-- 5 L1_Single_Bjet_50 : JET50
+l1_single_bjet_50 <= single_bjet_i5;
+algo(0) <= l1_single_bjet_50;
+
+-- ========================================================
+-- Instantiations conversions, calculations, etc.
+-- eta and phi conversion to muon scale for calo-muon and muon-esums correlation conditions (used for DETA, DPHI, DR and mass)
+
+-- pt, eta, phi, cosine phi and sine phi for correlation conditions (used for DETA, DPHI, DR, mass, overlap_remover and two-body pt)
+
+-- deta and dphi calculations for correlation conditions (used for DETA, DPHI)
+
+-- eta, dphi, cosh deta and cos dphi LUTs for correlation conditions (used for DR and mass)
+--
+-- Instantiations of correlation cuts calculations
+--
+-- Instantiations of DeltaEta LUTs
+
+-- Instantiations of DeltaPhi LUTs
+
+-- Instantiations of DeltaR calculation
+
+-- Instantiations of Invariant mass calculation
+
+-- Instantiations of Invariant mass divided DeltaR calculation
+
+-- Instantiations of Invariant mass unconstrained pt calculation
+
+-- Instantiations of Transverse mass calculation
+
+-- Instantiations of Two-body pt calculation
+
+-- muon charge correlations
+
+
+-- ========================================================
---
+++ 2023/L1Menu_cicada_test_v2-d1/vhdl/module_5/src/gtl_module_signals.vhd
@@ -0,0 +1,36 @@
+-- ========================================================
+-- from VHDL producer:
+
+-- Module ID: 5
+
+-- Name of L1 Trigger Menu:
+-- L1Menu_cicada_test
+
+-- Unique ID of L1 Trigger Menu:
+-- 60505f22-e28a-4665-b1ab-3248347defcb
+
+-- Unique ID of firmware implementation:
+-- bfef8aa9-e42b-4b0f-af95-c29f9411bc39
+
+-- Scale set:
+-- scales_2021_03_02
+
+-- VHDL producer version
+-- v2.14.0
+
+-- tmEventSetup version
+-- v0.10.0
+
+-- Signal definition of pt, eta and phi for correlation conditions.
+
+-- Signal definition for cuts of correlation conditions.
+
+-- Signal definition for muon charge correlations.
+
+-- Signal definition for conditions names
+    signal single_bjet_i5 : std_logic;
+
+-- Signal definition for algorithms names
+    signal l1_single_bjet_50 : std_logic;
+
+-- ========================================================
