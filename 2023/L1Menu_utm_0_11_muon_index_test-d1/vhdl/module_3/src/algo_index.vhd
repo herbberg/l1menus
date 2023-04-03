5d77c5b1f4d8c7bdf4d8136b8414190922db75ff
hbergaue <herbert.bergauer@oeaw.ac.at>
2023-04-03 11:51:51 +0200

    added test menu

---
+++ 2023/L1Menu_utm_0_11_muon_index_test-d1/vhdl/module_3/src/algo_index.vhd
@@ -0,0 +1,31 @@
+-- ========================================================
+-- from VHDL producer:
+
+-- Module ID: 3
+
+-- Name of L1 Trigger Menu:
+-- L1Menu_utm_0_11_muon_index_test
+
+-- Unique ID of L1 Trigger Menu:
+-- 817de68e-8d4f-4782-a9de-48a105220eb7
+
+-- Unique ID of firmware implementation:
+-- 84679211-9c26-43a6-a39b-6a3e7808b958
+
+-- Scale set:
+-- scales_2023_02_16
+
+-- VHDL producer version
+-- v2.14.0
+
+-- tmEventSetup version
+-- v0.11.2
+
+-- HB 2016-09-16: constants for algo_mapping_rop.
+type global_index_array is array (0 to NR_ALGOS-1) of integer;
+constant global_index: global_index_array := (
+          0, -- module_index: 0, name: L1_SingleMu_index_4_8
+    others => 0
+);
+
+-- ========================================================
---
+++ 2023/L1Menu_utm_0_11_muon_index_test-d1/vhdl/module_5/src/gtl_module_signals.vhd
@@ -0,0 +1,36 @@
+-- ========================================================
+-- from VHDL producer:
+
+-- Module ID: 5
+
+-- Name of L1 Trigger Menu:
+-- L1Menu_utm_0_11_muon_index_test
+
+-- Unique ID of L1 Trigger Menu:
+-- 817de68e-8d4f-4782-a9de-48a105220eb7
+
+-- Unique ID of firmware implementation:
+-- 84679211-9c26-43a6-a39b-6a3e7808b958
+
+-- Scale set:
+-- scales_2023_02_16
+
+-- VHDL producer version
+-- v2.14.0
+
+-- tmEventSetup version
+-- v0.11.2
+
+-- Signal definition of pt, eta and phi for correlation conditions.
+
+-- Signal definition for cuts of correlation conditions.
+
+-- Signal definition for muon charge correlations.
+
+-- Signal definition for conditions names
+    signal single_mu_i3 : std_logic;
+
+-- Signal definition for algorithms names
+    signal l1_single_mu_index_80_85 : std_logic;
+
+-- ========================================================
