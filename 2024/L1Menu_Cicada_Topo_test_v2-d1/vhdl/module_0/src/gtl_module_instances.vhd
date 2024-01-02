d41c26d272188125dba678abe5c4b7aa91ae1cbc
Herbert Bergauer <herbert.bergauer@oeaw.ac.at>
2024-01-02 10:25:02 +0100

    added new directory and menu

---
+++ 2024/L1Menu_Cicada_Topo_test_v2-d1/vhdl/module_0/src/gtl_module_instances.vhd
@@ -0,0 +1,82 @@
+-- ========================================================
+-- from VHDL producer:
+
+-- Module ID: 0
+
+-- Name of L1 Trigger Menu:
+-- L1Menu_Cicada_Topo_test_v2
+
+-- Unique ID of L1 Trigger Menu:
+-- e50b8093-a248-4fd5-baf6-5b197178654a
+
+-- Unique ID of firmware implementation:
+-- 63ec72cc-0abe-45b8-8547-f065da428dd1
+
+-- Scale set:
+-- scales_2023_12_14
+
+-- VHDL producer version
+-- v2.17.0
+
+-- tmEventSetup version
+-- v0.12.0
+
+-- ========================================================
+-- Instantiations of conditions
+--
+cond_anomaly_detection_trigger_i5_i: entity work.adt_wrapper
+    generic map(false, 20000)
+    port map(
+        lhc_clk,
+        bx_data.mu(2),
+        bx_data.eg(2),
+        bx_data.jet(2),
+        bx_data.tau(2),
+        bx_data.ett(2),
+        bx_data.htt(2),
+        bx_data.etm(2),
+        bx_data.htm(2),
+        bx_data.etmhf(2),
+        anomaly_detection_trigger_i5
+    );
+
+
+-- ========================================================
+-- Instantiations of algorithms
+
+-- 4 L1_Adt_20000 : ADT[ADT-ASCORE_20000]
+l1_adt_20000 <= anomaly_detection_trigger_i5;
+algo(0) <= l1_adt_20000;
+
+-- ========================================================
+-- Instantiations conversions, calculations, etc.
+-- eta and phi conversion to muon scale for calo-muon and muon-esums correlation conditions (used for DETA, DPHI, DR and mass)
+
+-- pt, eta, phi, cosine phi and sine phi for correlation conditions (used for DETA, DPHI, DR, mass, overlap_remover and two-body pt)
+
+-- deta and dphi calculations for correlation conditions (used for DETA, DPHI)
+
+-- eta, dphi, cosh deta and cos dphi LUTs for correlation conditions (used for DR and mass)
+--
+-- Instantiations of correlation cuts calculations
+--
+-- Instantiations of DeltaEta LUTs
+
+-- Instantiations of DeltaPhi LUTs
+
+-- Instantiations of DeltaR calculation
+
+-- Instantiations of Invariant mass calculation
+
+-- Instantiations of Invariant mass divided DeltaR calculation
+
+-- Instantiations of Invariant mass unconstrained pt calculation
+
+-- Instantiations of Transverse mass calculation
+
+-- Instantiations of Two-body pt calculation
+
+-- muon charge correlations
+
+
+-- ========================================================
