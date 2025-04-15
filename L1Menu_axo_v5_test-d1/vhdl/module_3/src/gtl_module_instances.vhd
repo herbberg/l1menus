346077c2bfbe5aed55a9c9f6c473c4df0da7be30
Herbert Bergauer <herbert.bergauer@oeaw.ac.at>
2025-04-15 15:22:55 +0200

    added new menu

---
+++ L1Menu_axo_v5_test-d1/vhdl/module_3/src/gtl_module_instances.vhd
@@ -0,0 +1,84 @@
+-- ========================================================
+-- from VHDL producer:
+
+-- Module ID: 3
+
+-- Name of L1 Trigger Menu:
+-- L1Menu_axo_v5_test
+
+-- Unique ID of L1 Trigger Menu:
+-- 4c1609a0-428f-46f5-a369-968c99708bd3
+
+-- Unique ID of firmware implementation:
+-- 4102063b-2572-422e-a0dc-bd230f723fff
+
+-- Scale set:
+-- scales_2024_05_15
+
+-- VHDL producer
+-- version: 2.21.0
+-- hash value: 75fafcd9f3ecfd946f75bb50ac42c198ee0a825140f50f33282d67107651cba6
+
+-- tmEventSetup
+-- version: 0.13.0
+
+-- ========================================================
+-- Instantiations of conditions
+--
+cond_axol1tl_trigger_i0: entity work.axol1tl_v5_wrapper
+    generic map(false, 2010)
+    port map(
+        lhc_clk,
+        bx_data.mu(2),
+        bx_data.eg(2),
+        bx_data.jet(2),
+        bx_data.tau(2),
+        bx_data.ett(2),
+        bx_data.htt(2),
+        bx_data.etm(2),
+        bx_data.htm(2),
+        bx_data.etmhf(2),
+        bx_data.htmhf(2),
+        axol1tl_trigger_i0
+    );
+
+
+-- ========================================================
+-- Instantiations of algorithms
+
+-- 0 L1_AXO_VLoose : AXO[AXO-MODEL_v5,AXO-SCORE_VLoose]
+l1_axo_v_loose <= axol1tl_trigger_i0;
+algo(0) <= l1_axo_v_loose;
+
+-- ========================================================
+-- Instantiations conversions, calculations, etc.
+-- eta and phi conversion to muon scale for calo-muon and muon-esums correlation conditions (used for DETA, DPHI, DR and mass)
+
+-- pt, eta, phi, cosine phi and sine phi for correlation conditions (used for DETA, DPHI, DR, mass, overlap_remover and two-body pt)
+
+-- deta and dphi calculations for correlation conditions (used for DETA, DPHI)
+
+-- eta, dphi, cosh deta and cos dphi LUTs for correlation conditions (used for DR and mass)
+--
+-- Instantiations of correlation cuts calculations
+--
+-- Instantiations of DeltaEta LUTs
+
+-- Instantiations of DeltaPhi LUTs
+
+-- Instantiations of DeltaR calculation
+
+-- Instantiations of Invariant mass calculation
+
+-- Instantiations of Invariant mass divided DeltaR calculation
+
+-- Instantiations of Invariant mass unconstrained pt calculation
+
+-- Instantiations of Transverse mass calculation
+
+-- Instantiations of Two-body pt calculation
+
+-- muon charge correlations
+
+
+-- ========================================================
